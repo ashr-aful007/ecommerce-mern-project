5d8a97b5439578ff5596976df6059174c18798b2
ashr-aful007 <ashraful99980@gmail.com>
2023-08-16 09:19:43 +0600

    err handlear and middleWare add

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 ## projects plan
 
+1. Projects plan
+2. Environment setup
+3. Create express server
+4. HTTP request & response
+5. nodemon and morgan package
+5. API testing with Postman
+7. Middleware
+
 ## Environment setup
 
 ## Express server setup
--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
 const express = require("express");
 const morgan = require('morgan')
 const app = express();
-app.use(morgan('dev'))
 const port = process.env.PORT || 5000;
+
+//middleWare,morgan,expressJson,urlencoded
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+//user Login handle middleWare
+const isLoggedIn = (req, res, next) =>{
+     const login = true;
+     if(login){
+          next()
+     }else{
+          return res.status(401).json({message: "plz login first"})
+     }
+}
+
 
 
 //route for products
-app.get("/test", (req, res) =>{
+app.get('/test', (req, res) =>{
      res.status(200).send({
-          message: "api is working fine"
+          message: "api is working fine",
+     })
+})
+
+//user profile
+app.get('/api/user',isLoggedIn, (req, res) =>{
+     res.status(200).send({
+          message: "user profile working"
      })
 })
 
@@ -20,7 +42,17 @@ app.get("/test", (req, res) =>{
 
 
 
+//client error handling
+app.use((req, res, next) =>{
+     res.status(404).json({message: "route not found"});
+     next()
+})
 
+//server error handling
+app.use((err, res, next) =>{
+     console.log(err.stack);
+     res.status(500).send("Something broke")
+})
 
 
 
