cca01c2f1bf6bb8d9c69e2977b4190fec8b047fe
ashr-aful007 <ashraful99980@gmail.com>
2023-09-06 08:10:34 +0600

    success fully add pagination

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -10,7 +10,7 @@ const getUsers = async(req, res, next) =>{
     //search functionality for users
     const search = req.query.search || "";
     const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) || 5;
+    const limit = Number(req.query.limit) || 1;
 
     const searchRegExp = new RegExp(".*" + search + ".*", 'i');
     
@@ -29,9 +29,18 @@ const getUsers = async(req, res, next) =>{
      
      //when we dont get any users
      if(!users) throw createError(404, 'no users found')
+
+     //send response for user searching 
      res.status(200).send({
           message: "user profile working",
           users,
+          pagination:{
+             totalPages: Math.ceil(count / limit),
+             currentPage: page,
+             previousPage: page - 1 > 0 ? page - 1 : null,
+             nextPage: page + 1 <= Math.ceil(count / limit) ? page + 1 : null,
+
+          }
      })
    } catch (error){
           next(error)
