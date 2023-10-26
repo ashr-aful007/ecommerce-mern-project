b088a17c40df9813f53f009f6ecdac7eac7f0c62
ashr-aful007 <ashraful99980@gmail.com>
2023-10-26 11:30:34 +0600

    auth check password

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,6 +2,7 @@ const createError = require('http-errors');
 const User = require("../models/userModel");
 const { successResponse } = require('./responseController');
 const {createJSONwebToken} = require('../helper/jsonwebtoken');
+const bcrypt = require('bcryptjs')
 
 
 
@@ -10,10 +11,28 @@ const handleLogin = async(req, res, next) =>{
           //email, password, req.body
           const {email, password} = req.body
           //isExist
-          const user = await User.findOne({email})
+          const user = await User.findOne({email});
+          if(!user){
+               throw createError(
+                    404, 'User does not exist with this email. Please register first'
+                    );
+          }
           //compare the password
+          const isPasswordMatch = await bcrypt.compare(password, user.password)
+          if(!isPasswordMatch){
+               throw createError(
+                    404, 
+                    'Email and password did not match'
+               )
+          }
           //isBanned
           //token, cookie
+          //send respsonse 
+          return successResponse(res, {
+               statusCode: 200,
+               message: 'users login successfully',
+               payload: {},
+          })
 
      } catch (error){
           next(error)
